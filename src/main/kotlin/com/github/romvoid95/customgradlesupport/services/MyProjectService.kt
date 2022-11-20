222c64b9742c1f7dd454e3869b3f8dd0f762f989
GitHub Action <action@github.com>
2022-11-20 23:20:00 +0000

    Template cleanup

--- src/main/kotlin/com/github/romvoid95/customgradlesupport/services/MyProjectService.kt
+++ src/main/kotlin/com/github/romvoid95/customgradlesupport/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.romvoid95.customgradlesupport.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.romvoid95.customgradlesupport.MyBundle
 
 class MyProjectService(project: Project) {
 
